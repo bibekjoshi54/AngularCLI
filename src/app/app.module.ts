fbf5c6a66242fe2e201932eea3f1ce22dba07989
Bibek Joshi <bibekjoshi54@gmail.com>
2017-06-27 14:18:15 +0545

    Created the module using ng g m <module name>

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,17 +7,21 @@ import { CustomerComponent } from './customer/customer.component';
 import { OrdersComponent } from './orders/orders.component';
 import { SearchBoxDirective } from './search-box.directive';
 import { SalesDataService } from 'app/sales-data.service';
+import { InitCapsPipe } from './shared/init-caps.pipe';
+import { LoginModule } from 'app/login/login.module';
 
 @NgModule({
   declarations: [
     AppComponent,
     CustomerComponent,
     OrdersComponent,
-    SearchBoxDirective
+    SearchBoxDirective,
+    InitCapsPipe
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    LoginModule
   ],
   providers: [SalesDataService],
   bootstrap: [AppComponent]
